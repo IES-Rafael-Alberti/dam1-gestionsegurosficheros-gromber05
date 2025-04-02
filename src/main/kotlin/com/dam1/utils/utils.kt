190a597336924632de502d5274180863106971d7
gromber05 <gromber2911@g.educaand.es>
2025-04-02 13:01:52 +0200

    Pequeño cambio en utils

--- src/main/kotlin/com/dam1/utils/utils.kt
+++ src/main/kotlin/com/dam1/utils/utils.kt
@@ -1,8 +1,11 @@
 package com.dam1.utils
 
 import com.dam1.ui.Consola
+import com.dam1.ui.Output
 
 object utils {
+
+    private val consola: Output = Consola()
 
     fun pedirDatos(valor: String): String {
         var opcion: String? = null
